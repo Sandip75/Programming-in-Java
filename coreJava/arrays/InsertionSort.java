86035d720c4be185d08fe221f24d391b98e4d257
Sandip75 <sk1957339@gmail.com>
2018-09-06 12:18:00 +0530

    insertionSort

---
+++ coreJava/arrays/InsertionSort.java
@@ -0,0 +1,30 @@
+package coreJava.arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 2, 17, -1, 5, 3 };
+		insertionSort(arr);
+		for (int val : arr) {
+			System.out.print(val + "  ");
+		}
+	}
+
+	public static void insertionSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (arr[j] < arr[j - 1]) {
+					swap(arr, j, j - 1);
+				} else {
+					break;
+				}
+			}
+		}
+	}
+
+	private static void swap(int[] arr, int i, int j) {
+		arr[i] = arr[i] + arr[j];
+		arr[j] = arr[i] - arr[j];
+		arr[i] = arr[i] - arr[j];
+	}
+}
--- coreJava/arrays/SelectionSort.java
+++ coreJava/arrays/SelectionSort.java
@@ -11,8 +11,8 @@ public class SelectionSort {
 	}
 
 	public static void selectionSort(int[] arr) {
-		for (int i = 0; i < arr.length-1; i++) {
-			for (int j = i+1; j < arr.length; j++) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] > arr[j]) {
 					swap(arr, i, j);
 				}
