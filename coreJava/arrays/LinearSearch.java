0cde8e77e4a9ef63baf5c0eb2dd29e9f17748c90
Sandip75 <sk1957339@gmail.com>
2018-09-06 09:21:16 +0530

    max

--- coreJava/arrays/LinearSearch.java
+++ coreJava/arrays/LinearSearch.java
@@ -16,5 +16,5 @@ public class LinearSearch {
 			}
 		}
 		return -1;
-	}
+	} 
 }
---
+++ coreJava/arrays/MaximumInArray.java
@@ -0,0 +1,25 @@
+package coreJava.arrays;
+
+import java.util.*;
+
+public class MaximumInArray {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0 ; i < arr.length ; i++){
+			arr[i] = scanner.nextInt();
+		}
+		
+		
+	}
+	
+	private static int maxArray(int[] arr){
+		
+		
+		return 0;
+	}
+
+}
