a9e4f30aee49ead112b5d7056a871d7aa572966e
Sandip75 <sk1957339@gmail.com>
2018-09-06 11:42:41 +0530

    bubbleSort

--- coreJava/arrays/BubbleSort.java
+++ coreJava/arrays/BubbleSort.java
@@ -11,8 +11,8 @@ public class BubbleSort {
 	}
 
 	public static void bubbleSort(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = 0; j < (arr.length - i - 1); j++) {
+		for (int i = 1; i < arr.length ; i++) {
+			for (int j = 0; j < (arr.length - i ); j++) {
 				if (arr[j] > arr[j + 1]) {
 					swap(arr, j, j + 1);
 				}
