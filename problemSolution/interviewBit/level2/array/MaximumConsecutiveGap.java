36ff58da9c3416d1c37ab4bb1890773f948dbad2
Sandip75 <sk1957339@gmail.com>
2018-09-16 10:07:56 +0530

    problemSolution

--- problemSolution/interviewBit/level2/array/MaximumConsecutiveGap.java
+++ problemSolution/interviewBit/level2/array/MaximumConsecutiveGap.java
@@ -1,10 +1,28 @@
 package problemSolution.interviewBit.level2.array;
 
+import java.util.*;
+
 public class MaximumConsecutiveGap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> l = new ArrayList<>();
+		l.add(1);
+		l.add(2);
+		l.add(5);
+		l.add(7);
+		int ans = maximumGap(l);
+		System.out.println(ans);
 	}
 
+	public static int maximumGap(final List<Integer> A) {
+		int ans =0;
+		Collections.sort(A);
+		for (int i = 1; i < A.size(); i++) {
+			int diff = A.get(i) - A.get(i-1);
+			if(diff > ans ){
+				ans = diff ;
+			}
+		}
+		return ans;
+	}
 }
