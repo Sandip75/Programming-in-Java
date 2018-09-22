045a6545dc7d5eb8882eca11779b0f087535870c
Sandip75 <sk1957339@gmail.com>
2018-09-22 23:21:26 +0530

    problemSolution

---
+++ problemSolution/math/PrimeSum.java
@@ -0,0 +1,40 @@
+package problemSolution.math;
+
+import java.util.*;
+
+public class PrimeSum {
+
+	public static void main(String[] args) {
+		int n = 40;
+		System.out.println(primeSum(n));
+	}
+
+	public static ArrayList<Integer> primeSum(int A) {
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		int l = 2;
+		int r = A - 2;
+		while (l <= r) {
+			if (isPrime(l) && isPrime(r)) {
+				ans.add(l);
+				ans.add(r);
+				break;
+			}
+			l++;
+			r--;
+		}
+		return ans;
+	}
+
+	private static boolean isPrime(int a) {
+
+		int i = 2;
+		while ((i * i) <= a) {
+			if (a % i == 0) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+
+}
