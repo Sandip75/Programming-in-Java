95bf55bb4b24509fac1cfc04d752042d1a149042
Sandip75 <sk1957339@gmail.com>
2018-09-27 21:56:32 +0530

    recursion

---
+++ coreJava/recursion/Array/AllIndex.java
@@ -0,0 +1,19 @@
+package coreJava.recursion.Array;
+
+public class AllIndex {
+
+	public static void main(String[] args) {
+		int arr[] = { 5, 44, 9, 1, 44, 18 };
+		int ans = allIndex(arr, 44, 0);
+		System.out.println(ans);
+	}
+	
+	public static int[] allIndex(int[] arr , int data ,int vidx , int count){
+		
+		
+		if(arr[vidx] == data){
+			
+		}
+	}
+
+}
--- coreJava/recursion/StartRecursion/TowerOfHanoi.java
+++ coreJava/recursion/StartRecursion/TowerOfHanoi.java
@@ -3,7 +3,9 @@ package coreJava.recursion.StartRecursion;
 public class TowerOfHanoi {
 
 	public static void main(String[] args) {
-		toh("S", "D", "H", 3);
+		// toh("S", "D", "H", 3);
+		int ans = tohCount("S", "D", "H", 3);
+		System.out.println(ans);
 	}
 
 	public static void toh(String src, String des, String help, int n) {
@@ -13,5 +15,15 @@ public class TowerOfHanoi {
 		toh(src, help, des, n - 1);
 		System.out.println(src + " to " + des + " --->>> " + n);
 		toh(help, des, src, n - 1);
+	}
+
+	public static int tohCount(String src, String des, String help, int n) {
+		if (n == 0) {
+			return 0;
+		}
+		int steps = tohCount(src, help, des, n - 1);
+		System.out.println(src + " to " + des + " --->>> " + n);
+		steps = steps + tohCount(help, des, src, n - 1);
+		return steps + 1;
 	}
 }
