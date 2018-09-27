006fb226fe2c1f8ed189a4812e7cceeea07509c6
Sandip75 <sk1957339@gmail.com>
2018-09-27 16:33:04 +0530

    recursion

--- coreJava/recursion/Array/FindFirstIndex.java
+++ coreJava/recursion/Array/FindFirstIndex.java
@@ -3,8 +3,35 @@ package coreJava.recursion.Array;
 public class FindFirstIndex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int arr[] = { 5, 44, 9, 1, 44, 18 };
+		int ans = findFirstIndex(arr, 44, 0);
+		System.out.println(ans);
+		int ans2 = findFirstIndexBetter(arr, 44, 0);
+		System.out.println(ans2);
+	}
 
+	public static int findFirstIndex(int[] arr, int data, int vidx) {
+		if (vidx == arr.length) {
+			return -1;
+		}
+
+		int ans = findFirstIndex(arr, data, vidx + 1);
+		if (data == arr[vidx]) {
+			return vidx;
+		} else {
+			return ans;
+		}
+	}
+
+	public static int findFirstIndexBetter(int[] arr, int data, int vidx) {
+		if (vidx == arr.length) {
+			return -1;
+		}
+		if (data == arr[vidx]) {
+			return vidx;
+		}
+		int ans = findFirstIndexBetter(arr, data, vidx + 1);
+		return ans;
 	}
 
 }
