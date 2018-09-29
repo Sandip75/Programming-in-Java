6c6f940891970d4888c3a9030db2fd1b8d1c03d1
Sandip75 <sk1957339@gmail.com>
2018-09-29 06:59:22 +0530

    string

---
+++ coreJava/string/PrintPalindromeSubstring.java
@@ -0,0 +1,35 @@
+package coreJava.string;
+
+public class PrintPalindromeSubstring {
+
+	public static void main(String[] args) {
+		String input = "naman";
+		printPalindromeSubstring(input);
+	}
+
+	public static void printPalindromeSubstring(String input) {
+		for (int i = 0; i < input.length(); i++) {
+			for (int j = i + 1; j <= input.length(); j++) {
+				String substring = input.substring(i, j);
+				boolean check = isPalindrome(substring);
+				if (check) {
+					System.out.println(substring);
+				}
+			}
+		}
+	}
+
+	private static boolean isPalindrome(String substring) {
+		int left = 0;
+		int right = substring.length() - 1;
+		while (left < right) {
+			if (substring.charAt(left) != substring.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
+
+}
