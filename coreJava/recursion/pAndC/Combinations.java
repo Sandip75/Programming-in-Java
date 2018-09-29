fa0c9364753fb1aa94ebb04ed4060c3d267c827f
Sandip75 <sk1957339@gmail.com>
2018-09-29 19:21:01 +0530

    recursion

--- coreJava/recursion/pAndC/Combinations.java
+++ coreJava/recursion/pAndC/Combinations.java
@@ -4,7 +4,8 @@ public class Combinations {
 
 	public static void main(String[] args) {
 		boolean[] boxes = new boolean[4];
-		combinations(boxes, 2, 0, -1, "");
+		// combinations(boxes, 2, 0, -1, "");
+		combinationsUsingRecursion(4, 0, 2, 0, "");
 	}
 
 	public static void combinations(boolean[] boxes, int titem, int citem, int lastbox, String asf) {
@@ -20,5 +21,22 @@ public class Combinations {
 				boxes[i] = false;
 			}
 		}
+	}
+
+	public static void combinationsUsingRecursion(int totalBox, int currentBox, int totalItem, int currentItem,
+			String asf) {
+
+		if (totalItem == currentItem) {
+			System.out.println(asf);
+			return;
+		}
+
+		if (totalBox == currentBox) {
+			return;
+		}
+
+		combinationsUsingRecursion(totalBox, currentBox + 1, totalItem, currentItem + 1,
+				asf + "q" + currentBox + "I" + currentItem + "  ");
+		combinationsUsingRecursion(totalBox, currentBox + 1, totalItem, currentItem, asf);
 	}
 }
--- coreJava/recursion/pAndC/PaymentCombination.java
+++ coreJava/recursion/pAndC/PaymentCombination.java
@@ -3,13 +3,13 @@ package coreJava.recursion.pAndC;
 public class PaymentCombination {
 
 	public static void main(String[] args) {
-		int[] coins = { 2, 3 , 5,6 };
+		int[] coins = { 2, 3, 5, 6 };
 		int amount = 10;
-		paymentCombination(coins, amount,  0, "");
+		paymentCombination(coins, amount, 0, "");
 
 	}
 
-	public static void paymentCombination(int[] coins, int tAmount, int coinIdx,  String asf) {
+	public static void paymentCombination(int[] coins, int tAmount, int coinIdx, String asf) {
 
 		if (tAmount == 0) {
 			System.out.println(asf);
@@ -17,10 +17,9 @@ public class PaymentCombination {
 		}
 
 		for (int i = coinIdx; i < coins.length; i++) {
-			if ( coins[i] <= tAmount) {
-				paymentCombination(coins, tAmount - coins[i], i,  asf + coins[i]);
+			if (coins[i] <= tAmount) {
+				paymentCombination(coins, tAmount - coins[i], i, asf + coins[i]);
 			}
 		}
 	}
-
 }
