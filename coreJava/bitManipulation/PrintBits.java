05aa181c048166b68f57708922cf6ea9e89c60ce
Sandip75 <sk1957339@gmail.com>
2018-09-30 13:54:46 +0530

    bit manipulation

---
+++ coreJava/bitManipulation/PrintBits.java
@@ -0,0 +1,21 @@
+package coreJava.bitManipulation;
+
+public class PrintBits {
+
+	public static void main(String[] args) {
+		int n = 57;
+		System.out.println(Integer.toBinaryString(n));
+		printBit(n);
+	}
+
+	public static void printBit(int n) {
+		for (int i = 31; i >= 0; i--) {
+			int mask = (1 << i);
+			if ((mask & n) == 0) {
+				System.out.print(0);
+			} else {
+				System.out.print(1);
+			}
+		}
+	}
+}
