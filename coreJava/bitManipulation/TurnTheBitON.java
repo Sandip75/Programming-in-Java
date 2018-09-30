84ec1570f9786b35a7949351e81414eef9ebd2b1
Sandip75 <sk1957339@gmail.com>
2018-09-30 13:31:30 +0530

    bit manipulation

---
+++ coreJava/bitManipulation/TurnTheBitON.java
@@ -0,0 +1,22 @@
+package coreJava.bitManipulation;
+
+public class TurnTheBitON {
+
+	public static void main(String[] args) {
+		int n = 57;
+		int k = 2;
+		System.out.println(Integer.toBinaryString(n));
+		int ans = turnTheBitON(n, k);
+		System.out.println(Integer.toBinaryString(ans));
+		System.out.println(ans);
+	}
+
+	public static int turnTheBitON(int n, int k) {
+
+		int mask = (1 << k);
+		System.out.println(Integer.toBinaryString(mask));
+		n = n | mask;
+		return n;
+	}
+
+}
--- coreJava/recursion/pAndC/Sudoku.java
+++ coreJava/recursion/pAndC/Sudoku.java
@@ -5,5 +5,6 @@ public class Sudoku {
 	public static void main(String[] args) {
 		
 	}
+	
 
 }
