504460e555b149bfdd829f295a11664f065d36e2
Sandip75 <sk1957339@gmail.com>
2018-10-10 21:16:07 +0530

    recursion

---
+++ coreJava/recursion/memory/SubsetWithTargetSum.java
@@ -0,0 +1,25 @@
+package coreJava.recursion.memory;
+
+public class SubsetWithTargetSum {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 10, 20, 30 };
+		int target = 30;
+		subsetWithTargetSum(arr, 0, target, 0, "");
+	}
+
+	public static void subsetWithTargetSum(int[] arr, int vidx, int target, int sum, String set) {
+		if (vidx == arr.length) {
+			if (target == sum) {
+				System.out.println(set);
+			}
+			return;
+		}
+
+		subsetWithTargetSum(arr, vidx + 1, target, sum + arr[vidx], set + arr[vidx] + " ");
+		subsetWithTargetSum(arr, vidx + 1, target, sum, set);
+
+	}
+
+}
