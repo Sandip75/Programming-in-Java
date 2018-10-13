72f0af074f499e455da5ec2a9cf999b934c374e9
Sandip75 <sk1957339@gmail.com>
2018-10-13 19:31:29 +0530

    dp

--- coreJava/dynamicProgramming/Maze.java
+++ coreJava/dynamicProgramming/Maze.java
@@ -3,18 +3,24 @@ package coreJava.dynamicProgramming;
 public class Maze {
 
 	public static void main(String[] args) {
-
+		System.out.println(mazeHVpathT(3, 3));
 	}
 
-	public static int mazeHVpathT(int sr, int sc, int dr, int dc) {
-		return 0;
+	public static int mazeHVpathT(int dr, int dc) {
+
+		int[][] ans = new int[dr + 1][dc + 1];
+
+		for (int i = dr; i >= 0; i--) {
+			for (int j = dc; j >= 0; j--) {
+				if (i == dr || j == dc) {
+					ans[i][j] = 1;
+				} else {
+					ans[i][j] = ans[i + 1][j] + ans[i][j + 1];
+				}
+			}
+		}
+
+		return ans[0][0];
 	}
 
-	public static int mazeHVDpathT(int sr, int sc, int dr, int dc) {
-		return 0;
-	}
-
-	public static int mazeMultiMovePathT(int sr, int sc, int dr, int dc) {
-		return 0;
-	}
 }
