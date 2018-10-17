6588fb557a7b923097ea6c2e4d04e5c1f77bf65d
Sandip75 <sk1957339@gmail.com>
2018-10-17 10:36:48 +0530

    problem

--- problemSolution/interviewBit/level3/bitManipulation/SingleNumber.java
+++ problemSolution/interviewBit/level3/bitManipulation/SingleNumber.java
@@ -1,9 +1,27 @@
 package problemSolution.interviewBit.level3.bitManipulation;
 
+import java.util.ArrayList;
+
 public class SingleNumber {
 
 	public static void main(String[] args) {
-
+		ArrayList<Integer> A = new ArrayList<>();
+		A.add(1);
+		A.add(3);
+		A.add(1);
+		A.add(3);
+		A.add(5);
+		int ans = singleNumber(A);
+		System.out.println(ans);
 	}
 
+	public static int singleNumber(final ArrayList<Integer> A) {
+		int ans = 0;
+
+		for (int i : A) {
+			ans = ans ^ i;
+		}
+
+		return ans;
+	}
 }
