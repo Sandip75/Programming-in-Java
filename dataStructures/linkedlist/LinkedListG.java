42f487ec4f265ee90db222625847c08371f786bc
Sandip75 <sk1957339@gmail.com>
2018-10-24 08:37:17 +0530

    linkedlist

---
+++ dataStructures/linkedlist/LinkedListG.java
@@ -0,0 +1,21 @@
+package dataStructures.linkedlist;
+
+public class LinkedListG {
+
+	private class Node {
+		int data;
+		Node next;
+	}
+
+	private Node head;
+	private Node tail;
+	private int size;
+
+	public int size() {
+		return this.size;
+	}
+
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+}
