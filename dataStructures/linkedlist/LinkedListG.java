24bd371459c0d5b5b8cf9505501e960c3c0adb81
Sandip75 <sk1957339@gmail.com>
2018-10-24 08:52:58 +0530

    linkedlist

--- dataStructures/linkedlist/LinkedListG.java
+++ dataStructures/linkedlist/LinkedListG.java
@@ -18,4 +18,38 @@ public class LinkedListG {
 	public boolean isEmpty() {
 		return this.size == 0;
 	}
+
+	public void addLast(int data) {
+		// when list is empty
+		if(this.size == 0){
+			this.handleAddWhenSize0(data);
+			return;
+		}
+		
+		// create
+		Node node = new Node();
+
+		// set properties
+		node.data = data;
+		node.next = null;
+
+		// attach
+		tail.next = node;
+
+		// summary update
+		tail = node;
+		this.size++;
+	}
+
+	private void handleAddWhenSize0(int data) {
+		Node node = new Node();
+		
+		node.data = data;
+		node.next = null ;
+		
+		this.head = this.tail = node ;
+		this.size++;
+	}
+	
+	
 }
