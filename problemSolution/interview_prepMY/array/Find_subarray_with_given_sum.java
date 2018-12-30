f432b9aec5d63c78ecfdc56ddc9e3aef49340da9
Sandip75 <sk1957339@gmail.com>
2018-12-30 11:01:13 +0530

    P

--- problemSolution/interview_prepMY/array/Find_subarray_with_given_sum.java
+++ problemSolution/interview_prepMY/array/Find_subarray_with_given_sum.java
@@ -1,3 +1,5 @@
+// https://www.geeksforgeeks.org/find-subarray-with-given-sum/
+
 package problemSolution.interview_prepMY.array;
 
 public class Find_subarray_with_given_sum {
@@ -6,22 +8,45 @@ public class Find_subarray_with_given_sum {
 		int[] arr = { 1, 4, 20, 3, 10, 5 };
 		int target = 5;
 		sum(arr, target);
+		sumBetter(arr, target);
 	}
 
 	public static void sum(int[] arr, int target) {
 		for (int i = 0; i < arr.length; i++) {
-			int tsum = 0 ;
+			int tsum = 0;
 			for (int j = i; j < arr.length; j++) {
 				tsum += arr[j];
-				if(tsum == target){
-					System.out.println(i + "  " +  j);
+				if (tsum == target) {
+					System.out.println(i + "  " + j);
 					return;
 				}
 			}
 		}
-		
+
 		System.out.println(-1);
 		return;
+	}
+
+	public static void sumBetter(int[] arr, int target) {
+
+		int[] ans = new int[arr.length];
+		ans[0] = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			ans[i] = arr[i] + ans[i - 1];
+		}
+
+		int traverse = 0;
+		for (int i = 0; i < ans.length; i++) {
+			if (target == ans[i]) {
+				System.out.println(0 + " " + i);
+				return;
+			} else if (target < ans[i]) {
+				traverse = i;
+				break;
+			}
+		}
+		
+		while(t)
 	}
 
 }
