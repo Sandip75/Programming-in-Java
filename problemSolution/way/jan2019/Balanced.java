d389005aa08690e2937ff06667eb1b0234de2ffb
Sandip75 <sk1957339@gmail.com>
2019-02-15 09:14:35 +0530

    Balanced

--- problemSolution/way/jan2019/Balanced.java
+++ problemSolution/way/jan2019/Balanced.java
@@ -7,27 +7,32 @@ public class Balanced {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		//String s = scn.next();
-		String s = "()()())";
+		String s = ")(";
 		
 		int check = 0 ; 
+		boolean flag = false ;
 		for(int i=0 ;  i< s.length() ; i++){
 			if(s.charAt(i) == '('){
 				check +=1;
 			}else {
 				check -=1;
+			}						
+			if(!flag && check==-1){
+				check +=1;
+				flag = true;
 			}			
 			if(check < -1){
 				System.out.println("No");
 				return ;
 			}			
-		}
-		
-		if(check==0 || check==-1 || check==1){
+		}		
+		if(flag && check==0){
 			System.out.println("Yes");
-		}else{
+		}else if(!flag && (check==0 || check==-1 || check==1)){
+			System.out.println("Yes");
+		}
+		else{
 			System.out.println("No");
 		}
-
 	}
-
 }
